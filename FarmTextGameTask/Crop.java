b67caadbc04a9004182e06d64669980645ad435e
KimDongHwan93 <dlwbtm77777@gmail.com>
2023-12-18 17:19:08 +0900

    add FarmTextGameTask

---
+++ FarmTextGameTask/Crop.java
@@ -0,0 +1,20 @@
+package 농장텍스트게임만들기;
+
+public abstract class Crop {
+
+    int no = 0;
+
+    int growth;
+    int price;
+
+    String name;
+
+
+
+          public void giveWaterCrop (){
+          }
+
+
+}
+
+
